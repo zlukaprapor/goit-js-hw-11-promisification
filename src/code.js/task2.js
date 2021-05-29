dd9a2c8b3aa954ac0abe57e7dd5a15bac6b8da2a
Oleksii Tkachenko <zlukapraporr@gmail.com>
2021-05-29 20:24:34 +0300

    1

---
+++ src/code.js/task2.js
@@ -0,0 +1,25 @@
+
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+  
+
+  const toggleUserState = (allUsers, userName) => {
+    return new Promise((resolve, reject) => {
+        resolve(allUsers.map(user =>
+                  user.name === userName ? { ...user, active: !user.active } : user,
+                ));
+    }); 
+  }
+
+  const logger = updatedUsers => console.table(updatedUsers);
+  
+  
+  /*
+   * Должно работать так
+   */
+  toggleUserState(users, 'Mango').then(logger);
+  toggleUserState(users, 'Lux').then(logger);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './sass/main.scss';
+import './code.js/task1';
+import './code.js/task2';
+import './code.js/task3';
